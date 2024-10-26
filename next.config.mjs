625a8b3ee29cd510d665616eac7e5af6748a3081
186526 <i@186526.xyz>
2024-10-26 21:55:55 +0800

    fix(buildId): add fallback when git not aval

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 import buildId from "next-build-id";
 import { dirname } from "node:path";
+import { env } from "node:process";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -18,8 +19,11 @@ const nextConfig = {
 		// !! WARN !!
 		ignoreBuildErrors: true,
 	},
-	generateBuildId: async () =>
-		await buildId({ dir: __dirname, describe: true }),
+	generateBuildId: async () => {
+		return await buildId({ dir: __dirname, describe: true }).catch((e) => {
+			return env.NODE_ENV === "production" ? "production" : "development";
+		});
+	},
 	output: "standalone",
 };
 
